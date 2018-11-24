8a2cae157825e596054676e40b914980966b82e6
MarchTala <marchjigtala@yahoo.com>
2018-11-24 13:02:53 +0800

    Add files via upload

---
+++ company_industries.sql
@@ -0,0 +1,76 @@
+CREATE TABLE `company_industries` (
+`id` int(11) NOT NULL auto_increment,
+`industry_name` varchar(100) NOT NULL default '',
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+-- 
+-- Dumping data for table `maintenance_countries`
+-- 
+INSERT INTO `company_industries` VALUES (null, 'Accounting / Audit / Tax Services');
+INSERT INTO `company_industries` VALUES (null, 'Advertising & Marketing & Promotion / PR');
+INSERT INTO `company_industries` VALUES (null, 'Agricultural / Animal / Live Stock / Fisheries');
+INSERT INTO `company_industries` VALUES (null, 'Airline / Aviation');
+INSERT INTO `company_industries` VALUES (null, 'Apparel');
+INSERT INTO `company_industries` VALUES (null, 'Architectural Services / Interior Design');
+INSERT INTO `company_industries` VALUES (null, 'Arts / Design / Fashion');
+INSERT INTO `company_industries` VALUES (null, 'Automobile / Vehicle');
+INSERT INTO `company_industries` VALUES (null, 'Banking and Financial Services');
+INSERT INTO `company_industries` VALUES (null, 'Beauty / Fitness');
+INSERT INTO `company_industries` VALUES (null, 'Biotechnology / Pharmaceutical / Clinical Research');
+INSERT INTO `company_industries` VALUES (null, 'Call Center / IT-Enabled Services / BPO');
+INSERT INTO `company_industries` VALUES (null, 'Cargo / Freight Services / Logistics');
+INSERT INTO `company_industries` VALUES (null, 'Chemical / Fertilizers / Pesticides');
+INSERT INTO `company_industries` VALUES (null, 'Computer / Information Technology (Hardware)');
+INSERT INTO `company_industries` VALUES (null, 'Computer / Information Technology (Software)');
+INSERT INTO `company_industries` VALUES (null, 'Construction / Building / Engineering');
+INSERT INTO `company_industries` VALUES (null, 'Consulting (IT, Engineering, Science & Technical)');
+INSERT INTO `company_industries` VALUES (null, 'Consulting (Science, Engineering & Technical)');
+INSERT INTO `company_industries` VALUES (null, 'Consumer Products / FMCG');
+INSERT INTO `company_industries` VALUES (null, 'Education & Training Services');
+INSERT INTO `company_industries` VALUES (null, 'Electronics & Semiconductors');
+INSERT INTO `company_industries` VALUES (null, 'Energy (Electrical Power & Petroleum)');
+INSERT INTO `company_industries` VALUES (null, 'Entertainment / Media');
+INSERT INTO `company_industries` VALUES (null, 'Environment / Health / Safety');
+INSERT INTO `company_industries` VALUES (null, 'Food & Beverage / Catering / Restaurant');
+INSERT INTO `company_industries` VALUES (null, 'General & Wholesale Trading');
+INSERT INTO `company_industries` VALUES (null, 'Government');
+INSERT INTO `company_industries` VALUES (null, 'HealthCare / Medical');
+INSERT INTO `company_industries` VALUES (null, 'Heavy Industrial / Machinery / Equipment');
+INSERT INTO `company_industries` VALUES (null, 'Hotel / Hospitality');
+INSERT INTO `company_industries` VALUES (null, 'Human Resources / Manpower & Recruitment');
+INSERT INTO `company_industries` VALUES (null, 'Insurance');
+INSERT INTO `company_industries` VALUES (null, 'Journalism');
+INSERT INTO `company_industries` VALUES (null, 'Law / Legal');
+INSERT INTO `company_industries` VALUES (null, 'Library / Museum');
+INSERT INTO `company_industries` VALUES (null, 'Management');
+INSERT INTO `company_industries` VALUES (null, 'Manufacturing / Production');
+INSERT INTO `company_industries` VALUES (null, 'Marine / Aquaculture');
+INSERT INTO `company_industries` VALUES (null, 'Mining & Quarrying');
+INSERT INTO `company_industries` VALUES (null, 'Non-Profit Organization / Social Services / NGO');
+INSERT INTO `company_industries` VALUES (null, 'Oil / Gas');
+INSERT INTO `company_industries` VALUES (null, 'Others');
+INSERT INTO `company_industries` VALUES (null, 'Pharmaceutical');
+INSERT INTO `company_industries` VALUES (null, 'Photgraphy / Videography');
+INSERT INTO `company_industries` VALUES (null, 'Plantations');
+INSERT INTO `company_industries` VALUES (null, 'Polymer / Plastic / Rubber / Tyres');
+INSERT INTO `company_industries` VALUES (null, 'Printing / Publishing');
+INSERT INTO `company_industries` VALUES (null, 'Property / Real State');
+INSERT INTO `company_industries` VALUES (null, 'Publishing');
+INSERT INTO `company_industries` VALUES (null, 'Recycling & Waste Management');
+INSERT INTO `company_industries` VALUES (null, 'Repair & Maintenance Services');
+INSERT INTO `company_industries` VALUES (null, 'Research & Development');
+INSERT INTO `company_industries` VALUES (null, 'Retail / Merchandise');
+INSERT INTO `company_industries` VALUES (null, 'Science & Technology');
+INSERT INTO `company_industries` VALUES (null, 'Security & Protection Services');
+INSERT INTO `company_industries` VALUES (null, 'Semiconductor / Wafer Fabrication');
+INSERT INTO `company_industries` VALUES (null, 'Shipping / Shipbuilding');
+INSERT INTO `company_industries` VALUES (null, 'Sports & Recreation');
+INSERT INTO `company_industries` VALUES (null, 'Steel Fabrication');
+INSERT INTO `company_industries` VALUES (null, 'Stockbroking / Securities');
+INSERT INTO `company_industries` VALUES (null, 'Telecommunication');
+INSERT INTO `company_industries` VALUES (null, 'Textiles / Garment');
+INSERT INTO `company_industries` VALUES (null, 'Tobacco');
+INSERT INTO `company_industries` VALUES (null, 'Transportation Services');
+INSERT INTO `company_industries` VALUES (null, 'Travel / Tourism');
+INSERT INTO `company_industries` VALUES (null, 'Utilities');
+INSERT INTO `company_industries` VALUES (null, 'Wood & Fibre');
